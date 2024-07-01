685484ee1bb2fc899cd98eb55a91c3b5cd7b85d2
neres <neresdev@gmail.com>
2024-07-01 13:24:43 -0300

    new project (chess) and Postion class

---
+++ languages/java/chess-project/src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Position position = new Position(3, 5);
+        System.out.println(position);
+    }
+}
