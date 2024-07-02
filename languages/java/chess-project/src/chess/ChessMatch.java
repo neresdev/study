75553daba0a04e7e5e7150d3b0f790c122aee869
neres <neresdev@gmail.com>
2024-07-02 20:05:33 -0300

    Printing possible moves

--- languages/java/chess-project/src/chess/ChessMatch.java
+++ languages/java/chess-project/src/chess/ChessMatch.java
@@ -25,6 +25,12 @@ public class ChessMatch {
         return mat;
     }
 
+    public boolean[][] possibleMoves(ChessPostition sourcePosition){
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+
     public ChessPiece performChessMove(ChessPostition sourcePosition, ChessPostition targetPosition){
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
--- languages/java/chess-project/src/chess/pieces/Rook.java
+++ languages/java/chess-project/src/chess/pieces/Rook.java
@@ -60,6 +60,6 @@ public class Rook extends ChessPiece{
             mat[p.getRow()][p.getColumn()] = true;
         }
 
-        return mat ;
+        return mat;
     }
 }
