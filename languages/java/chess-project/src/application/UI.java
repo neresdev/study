71839e96f804d1de9a61c64d8b8ad19dde689c47
neres <neresdev@gmail.com>
2024-07-05 15:00:59 -0300

    Piece move count

--- languages/java/chess-project/src/application/UI.java
+++ languages/java/chess-project/src/application/UI.java
@@ -12,6 +12,8 @@ import chess.Color;
 
 public class UI {
 
+    private UI(){}
+    
     public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
