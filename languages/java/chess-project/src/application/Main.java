7a6b409c0460e7fdc6785e96a68b0a8fe0f76802
neres <neresdev@gmail.com>
2024-07-05 13:58:37 -0300

    Checkmate logic

--- languages/java/chess-project/src/application/Main.java
+++ languages/java/chess-project/src/application/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
         List<ChessPiece> captured = new ArrayList<>();
                 
-        while(true){
+        while(!chessMatch.getCheckMate()){
             try{
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -44,5 +44,7 @@ public class Main {
                 sc.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
