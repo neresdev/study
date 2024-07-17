f5a97bb66547cee7323378f3d224439726da3946
neres <neresdev@gmail.com>
2024-07-17 14:04:02 -0300

    Chess project ending

--- languages/java/chess-project/src/chess/ChessMatch.java
+++ languages/java/chess-project/src/chess/ChessMatch.java
@@ -22,7 +22,7 @@ public class ChessMatch {
 	private boolean check;
 	private boolean checkMate;
 	private ChessPiece enPassantVulnerable;
-	
+	private ChessPiece promoted;
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
 	
@@ -51,6 +51,10 @@ public class ChessMatch {
 
 	public ChessPiece getEnPassantVulnerable(){
 		return enPassantVulnerable;
+	}
+
+	public ChessPiece getPromoted(){
+		return promoted;
 	}
 	
 	public ChessPiece[][] getPieces() {
@@ -83,6 +87,14 @@ public class ChessMatch {
 
 		ChessPiece movedPiece = (ChessPiece)board.piece(target);
 
+		// #specialmove promotion
+		promoted = null;
+		if(movedPiece instanceof Pawn && (movedPiece.getColor() == Color.WHITE && target.getRow() == 0 || movedPiece.getColor() == Color.BLACK && target.getRow() == 7)){
+			
+				promoted = (ChessPiece)board.piece(target);
+				promoted = replacePromotedPiece("Q");
+			
+		}
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 
 		if (testCheckMate(opponent(currentPlayer))) {
@@ -100,6 +112,31 @@ public class ChessMatch {
 		}
 		
 		return (ChessPiece)capturedPiece;
+	}
+
+	public ChessPiece replacePromotedPiece(String type){
+		if(promoted == null){
+			throw new IllegalStateException("There is no piece to be promoted");
+		}
+		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
+			return promoted;
+		}
+		Position pos = promoted.getChessPosition().toPosition();
+		Piece p = board.removePiece(pos);
+		piecesOnTheBoard.remove(p);
+
+		ChessPiece newPiece = newPiece(type, promoted.getColor());
+		board.placePiece(newPiece, pos);
+		piecesOnTheBoard.add(newPiece);
+		return newPiece;
+
+	}
+
+	private ChessPiece newPiece(String type, Color color){
+		if(type.equals("B")) return new Bishop(board, color);
+		if(type.equals("N")) return new Knight(board, color);
+		if(type.equals("Q")) return new Queen(board, color);
+		return new Bishop(board, color);
 	}
 	
 	private Piece makeMove(Position source, Position target) {
@@ -281,11 +318,12 @@ public class ChessMatch {
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE, this));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE, this));
+
         placeNewPiece('c', 2, new Pawn(board, Color.WHITE, this));
         placeNewPiece('d', 2, new Pawn(board, Color.WHITE, this));
         placeNewPiece('e', 2, new Pawn(board, Color.WHITE, this));
         placeNewPiece('f', 2, new Pawn(board, Color.WHITE, this));
-        placeNewPiece('g', 2, new Pawn(board, Color.WHITE, this));
+        placeNewPiece('g', 7, new Pawn(board, Color.WHITE, this)); // todo rollback
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE, this));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
@@ -302,7 +340,7 @@ public class ChessMatch {
         placeNewPiece('d', 7, new Pawn(board, Color.BLACK, this));
         placeNewPiece('e', 7, new Pawn(board, Color.BLACK, this));
         placeNewPiece('f', 7, new Pawn(board, Color.BLACK, this));
-        placeNewPiece('g', 7, new Pawn(board, Color.BLACK, this));
+        // placeNewPiece('g', 7, new Pawn(board, Color.BLACK, this)); // rollback!
         placeNewPiece('h', 7, new Pawn(board, Color.BLACK, this));
 	}
 }
