eba61b6c4217d288573d87b6777e292ee4fd3373
neres <neresdev@gmail.com>
2024-09-21 15:22:35 -0300

    project for data structure - wip: creating task comparator

--- languages/java/challenges/data-structure/project/src/app/App.java
+++ languages/java/challenges/data-structure/project/src/app/App.java
@@ -2,16 +2,33 @@ package app;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 
+import entities.enm.Priority;
+import entities.task.Task;
+import entities.task.TaskComparator;
 import service.TaskService;
 
 public class App {
 
-    public static void main(String[] args){
+    /* public static void main(String[] args){
         var taskService = new TaskService();
         taskService.run();
+    } */
+
+    public static void main(String[] args){
+        PriorityQueue<Task> priorityQueue = new PriorityQueue<>(2, new TaskComparator());
+        var lowTask = new Task("low task", "low", "pending");
+        var mediumTask = new Task("medium task", "medium", "pending");
+        var hightTask = new Task("hight task", "hight", "pending");
+        
+        priorityQueue.offer(lowTask);
+        priorityQueue.offer(mediumTask);
+        priorityQueue.offer(hightTask);
+
+        var comparator = new TaskComparator();
+
+        int a = 0;
     }
-
-
     
 }
--- languages/java/challenges/data-structure/project/src/entities/task/TaskComparator.java
+++ languages/java/challenges/data-structure/project/src/entities/task/TaskComparator.java
@@ -2,10 +2,21 @@ package entities.task;
 
 import java.util.Comparator;
 
+import entities.enm.Priority;
+
 public class TaskComparator implements Comparator<Task>{
     
+    // @Override
+    // public int compare(Task task1, Task task2){
+    //     return task1.getPriority().compareTo(task2.getPriority());
+    // }
+
     @Override
     public int compare(Task task1, Task task2){
-        return task1.getPriority().compareTo(task2.getPriority());
+        if(task1.getPriority() == Priority.HIGHT) return -1; // insere primeiro
+        else if( (task1.getPriority() == Priority.MEDIUM) &&  (task2.getPriority() == Priority.HIGHT)) return 1; // insere depois
+        else if( (task1.getPriority() == Priority.MEDIUM) &&  (task2.getPriority() == Priority.MEDIUM)) return 1; // insere depois
+        else if( (task1.getPriority() == Priority.MEDIUM) &&  (task2.getPriority() == Priority.LOW)) return -0; // insere no final
+        else return 0;
     }
 }
