2f54e22645abb4f1a4fe7454b749a7c0cbbb264c
neresdev <neresdev@gmail.com>
2026-03-17 18:05:48 -0300

    first version of Args class

--- languages/java/args-example-chapter-14-clean-code/src/main/java/org/example/exceptions/ArgsException.java
+++ languages/java/args-example-chapter-14-clean-code/src/main/java/org/example/exceptions/ArgsException.java
@@ -1,4 +1,7 @@
 package org.example.exceptions;
 
-public class ArgsException {
+public class ArgsException extends Exception {
+    private char errorArgumentId = '\0';
+    private String errorParameter = null;
+//    private ErrorCode errorCode = OK;
 }
