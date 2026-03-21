9551437552933ac4445df07211ac31dd313dc45f
neresdev <neresdev@gmail.com>
2026-03-20 21:35:14 -0300

    Main class using Args class

--- languages/java/args-example-chapter-14-clean-code/src/main/java/org/example/Main.java
+++ languages/java/args-example-chapter-14-clean-code/src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.text.ParseException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("vamo q vamo");
+    public static void main(String[] args) throws ParseException {
+        final var arg = new Args("l,p#,d*", args);
+        final var logging = arg.getBoolean('l');
     }
 }
